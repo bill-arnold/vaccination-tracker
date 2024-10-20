0e0852e3120fb24428d358c56e45c92b6d34de04
bill-arnold <bill.arnold@student.moringaschool.com>
2024-10-20 21:30:02 +0300

    added dotenv

--- app.js
+++ app.js
@@ -149,6 +149,11 @@ app.post('/logout', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
+//app.listen(3000, () => {
+//console.log('Server running at http://localhost:3000');
+//});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
+
